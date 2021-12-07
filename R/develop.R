27adc6e73f768178e6bdf24ec21d9be9477b6d07
Sean Raffuse <sraffuse@ucdavis.edu>
2021-12-07 12:18:49 -0800

    Support archived purpleair data and predict at test locations

--- R/develop.R
+++ R/develop.R
@@ -3,9 +3,10 @@
 
 # This function requires MAIAC and NARR data to be downloaded already
 develop_model <- function(dt1, dt2, states, pa_cutoff = 100000, seed = 1977,
-                          period = NULL) {
+                          bluesky_special = NULL, pa_data = NULL) {
 
   # Get and prep AirNow and AirSIS data
+  print("AirNow and AirSIS data...")
   an_ws <- get_airnow_daterange(dt1, dt2, states) %>%
     recast_monitors()
   as_ws <- get_airsis_daterange(dt1, dt2, states) %>%
@@ -13,36 +14,57 @@ develop_model <- function(dt1, dt2, states, pa_cutoff = 100000, seed = 1977,
   mon <- rbind(an_ws, as_ws)
 
   an_vg <- create_airnow_variograms(mon)
-  mon_split <- split_airnow_data(mon)
+  # trying a larger fraction of test data to make a more complete RF model
+  mon_split <- split_airnow_data(mon, test_fraction = 0.3)
   an_ok <- krige_airnow(mon_split, an_vg)
 
+  ank_complete <- krige_airnow_all(mon, mon, an_vg) %>%
+    select(monitorID, Day, PM25_log_ANK, PM25_log_var) %>%
+    distinct()
 
   # Prep Bluesky For 2020, the grid changed midway through october - will need
   # to process them separately. I added this hack to address it
-  if (period == "2020") {
-    bluesky_stack1 <- stack_bluesky_archive(as.Date("2020-08-01"), as.Date("2020-10-09"))
-    bluesky_stack2 <- stack_bluesky_archive(as.Date("2020-10-10"), as.Date("2020-10-31"))
-    mon1 <- mon[mon@data$Day <= as.POSIXct("2020-10-09"),]
-    mon2 <- mon[mon@data$Day > as.POSIXct("2020-10-09"),]
-    ## YOU ARE HERE - The extract in here is not working properly
-    bluesky1 <- preprocessed_bluesky_at_airnow(bluesky_stack1, mon1)
-    bluesky2 <- preprocessed_bluesky_at_airnow(bluesky_stack2, mon2)
-    bluesky <- bind_rows(bluesky1, bluesky2)
+  print("BlueSky data...")
+  if (!is.null(bluesky_special)) {
+    if (bluesky_special == "2020") {
+      bluesky_stack1 <- stack_bluesky_archive(dt1, as.Date("2020-10-09"))
+      bluesky_stack2 <- stack_bluesky_archive(as.Date("2020-10-10"), dt2)
+      mon1 <- mon[mon@data$Day <= as.POSIXct("2020-10-09"),]
+      mon2 <- mon[mon@data$Day > as.POSIXct("2020-10-09"),]
+      bluesky1 <- preprocessed_bluesky_at_airnow(bluesky_stack1, mon1)
+      bluesky2 <- preprocessed_bluesky_at_airnow(bluesky_stack2, mon2)
+      bluesky <- bind_rows(bluesky1, bluesky2)
+    } else if (bluesky_special == "HAQAST") {
+      #Use the HAQAST S1 CMAQ run instead of the standard Bluesky archive
+      bluesky_stack <- stack_haqast_archive(dt1, dt2)
+      bluesky <- preprocessed_bluesky_at_airnow(bluesky_stack, mon)
+    } else {
+      stop(paste("bluesky_special:", period, "not supported"))
+    }
   } else {
     bluesky_stack <- stack_bluesky_archive(dt1, dt2)
     bluesky <- preprocessed_bluesky_at_airnow(bluesky_stack, mon)
   }
 
   # Prep MAIAC AOD
+  print("Preparing MAIAC...")
   maiac <- maiac_at_airnow(mon)
 
   # Prep NARR
+  print("Preparing NARR...")
   narr <- narr_at_airnow(mon)
 
-  # Get and prep PurpleAir data
-  # For dates prior to 2019, the data must first be acquired by create_purpleair_archive
-  pa <- get_purpleair_daterange(dt1, dt2, states)
-  pas <- purpleair_spatial(pa)
+  # Get and prep PurpleAir data For dates prior to April 5, 2019, the data must
+  # be acquired by create_purpleair_archive before running this script
+  print("PurpleAir data...")
+  if (is.null(pa_data)) {
+    pa_data <- get_purpleair_daterange(dt1, dt2, states)
+  } else {
+    pa_data <- readRDS(pa_data) %>%
+      filter(Date >= dt1,
+             Date <=dt2)
+  }
+  pas <- purpleair_spatial(pa_data)
   pa_clean <- purpleair_clean_spatial_outliers(pas)
   pa_vg <- create_purpleair_variograms(pa_clean, cutoff = pa_cutoff)
 
@@ -50,6 +72,7 @@ develop_model <- function(dt1, dt2, states, pa_cutoff = 100000, seed = 1977,
   pa_ok <- krige_purpleair(pa_clean, mon, pa_vg)
 
   # Now need to put altogether in prep for RF modeling
+  print("Combining inputs for modeling...")
   train <- mon_split$train@data %>%
     mutate(Split = "Train")
   test <- mon_split$test@data %>%
@@ -110,9 +133,19 @@ develop_model <- function(dt1, dt2, states, pa_cutoff = 100000, seed = 1977,
 
   print(model)
 
-  # possibly save these model results?
-  # saveRDS(model, "./data/models/xxxxx")
-  return(model)
+  # Now, krige all the monitor sites and make a prediction at each. Then return
+  # both the model and the data frame including the inputs and prediction.
+  output <- bind_rows(train, test) %>%
+    left_join(ank_complete, by = c("monitorID", "Day")) %>%
+    left_join(pak_in, by = c("monitorID", "Day")) %>%
+    left_join(narr_in, by = c("monitorID", "Day")) %>%
+    left_join(bluesky_in, by = c("monitorID", "Day")) %>%
+    left_join(maiac_in, by = c("monitorID", "Day")) %>%
+    filter(!is.na(PM25_bluesky)) %>%
+    filter(if_all(where(is.numeric), is.finite))
+
+  output$PM25_log_RF <- predict(model$finalModel, output)
+  list(model = model, output=output)
 
 }
 
